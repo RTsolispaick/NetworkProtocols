45e283960d85ceb97aad0413fa799454bcf275af
RTsoli <robotort2140@gmail.com>
2024-05-03 21:42:21 +0500

    fix dns сервера

--- DNS_server/resources/dependencies.py
+++ DNS_server/resources/dependencies.py
@@ -2,6 +2,6 @@ import json
 
 
 def get_server_settings() -> dict:
-    with open("C:/Users/User/PycharmProjects/Network_protocols/DNS_server/resources/config.json", "r") as jsonfile:
+    with open("resources/config.json", "r") as jsonfile:
         settings = json.load(jsonfile)
     return settings
